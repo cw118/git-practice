3b57e25810f7178d6c2d0b7c688c89eef7459549
cw118 <carolyn94118@gmail.com>
2022-02-22 20:07:17 -0500

    Add shell scripting basics to .sh example

--- examples/script.sh
+++ examples/script.sh
@@ -10,3 +10,39 @@ then
 else
     echo "Hello there, $NAME. Have a good day!"
 fi
+
+# FILE CONDITIONS
+FILE="file.txt"
+
+if [ -f "$FILE" ]
+then
+    echo "$FILE is an existing file:"
+elif [ -d "$FILE" ]
+then
+    echo "$FILE is an existing directory"
+else
+    echo "$FILE is not a file or directory"
+fi
+
+# CASE STATEMENT AND OPENING A FILE
+read -p "Would you like to edit this shell script? (Y/n) " ANS
+
+case "$ANS" in
+    [yY] | [yY][eE][sS])
+        echo "Opening the script..."
+        nano script.sh
+        ;;
+    [nN] | [nN][oO])
+        echo "Continuing..."
+        ;;
+    *)
+        echo "Please enter Y/yes or n/no"
+        ;;
+esac
+
+# FOR LOOP
+NAMES="Ron Bill Fred George Ginny Percy Charlie"
+for NAME in $NAMES
+    do
+        echo "$NAME is a Weasley!"
+done
